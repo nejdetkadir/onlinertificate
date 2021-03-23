b9ac60b78d9de17de691933c0af6e249b59e1274
nejdetkadir <nejdetkadir.550@gmail.com>
2021-03-24 00:20:54 +0300

    Add validations of event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   mount_uploader :cover, ImageUploader
+
+  validates :name, presence: true, length: {minimum: 3}
+  validates :description, presence: true, length: {minimum: 10}
+  validates :cover, presence: :true
+  validates :event_date, presence: :true
+  validates :url, presence: :true
 end
